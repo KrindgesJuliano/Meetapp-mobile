0533a5a72ff5b0e938a51445aa6859b4219c33ff
Juliano Krindges <juliano9krindges@hotmail.com>
2019-10-24 19:56:48 -0300

    feat: create Input and Button components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Text} from 'react-native';
+
+import Input from '~/components/Input';
+import Buttons from '~/components/Buttons';
+
+import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <View />;
+  return (
+    <Background>
+      <Text>SignIn</Text>
+
+      <Input
+        style={{marginTop: 30}}
+        icon="call"
+        placeholder="digite seu nome"
+      />
+
+      <Buttons>Entrar</Buttons>
+    </Background>
+  );
 }
